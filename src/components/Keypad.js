f70e8dabd0c0406d11bd0c37eedaa27b1d04f77c
Katrina Pritchard <pritckat@gmail.com>
2018-11-10 04:04:40 +0000

    automatically backed up by learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
-// Code Keypad Component Here
+import React from 'react';
+
+class Keypad extends React.Component {
+
+
+  render() {
+    return(
+      <div>
+        <input type="password" />
+      </div>
+    )
+  }
+}
